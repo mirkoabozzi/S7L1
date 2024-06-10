f9d0e250faefe8fb0fb1b59c0dbf7de3a26e3f96
Mirko Abozzi <mirko.abozzi@gmail.com>
2024-06-10 15:35:10 +0200

    Es 2 class+ function

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+console.log("Esercizio 1");
 class User {
   constructor(firstName, lastName, age, location) {
     this.firstName = firstName;
@@ -20,3 +21,39 @@ const user2 = new User("Mario", "Rossi", 50, "Lazio");
 console.log(user1);
 console.log(user2);
 console.log(user1.ageCompare(user2));
+
+console.log("Esercizio 2");
+
+class Pet {
+  constructor(petName, ownerName, species, breed) {
+    this.petName = petName;
+    this.ownerName = ownerName;
+    this.species = species;
+    this.brees = breed;
+  }
+  ownerCompare(owner) {
+    if (this.ownerName === owner) {
+      return "i due animali hanno lo stsso padrone";
+    } else {
+      return "i padroni sono diversi";
+    }
+  }
+}
+
+const form = document.getElementById("petForm");
+console.log(form);
+
+form.onsubmit = function (event) {
+  event.preventDefault();
+
+  const petName = document.getElementById("petName").value;
+  const ownerName = document.getElementById("ownerName").value;
+  const species = document.getElementById("species").value;
+  const breed = document.getElementById("breed").value;
+  console.log(petName);
+  console.log(ownerName);
+  console.log(species);
+  console.log(breed);
+  const newPet = new Pet(petName, ownerName, species, breed);
+  console.log(newPet);
+};
